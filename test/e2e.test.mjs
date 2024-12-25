79f8c8c17269140d015041314cd2ba12150e4519
daido1976 <daido1976@gmail.com>
2024-12-25 11:25:21 +0900

    Add POST request to e2e test

--- test/e2e.test.mjs
+++ test/e2e.test.mjs
@@ -1,12 +1,12 @@
 import { expect, test } from "vitest";
 
-test("E2E Test", async () => {
-  const response = await fetch(
-    "http://localhost:8080/foo/bar?testkey=testvalue"
-  );
+const TEST_ENDPOINT = "http://localhost:8080/foo/bar";
+
+test("E2E Test for GET Request", async () => {
+  const response = await fetch(`${TEST_ENDPOINT}?testkey=testvalue`);
   const responseData = await response.json();
 
-  // Expected JSON (dynamic fields replaced with expectations)
+  // Expected JSON for GET request
   const expected = {
     message: "Hello from Lambda!",
     event: {
@@ -16,7 +16,7 @@ test("E2E Test", async () => {
       rawQueryString: "testkey=testvalue",
       headers: expect.objectContaining({
         Accept: "*/*",
-        "User-Agent": expect.any(String), // User-Agent の形式を検証
+        "User-Agent": expect.any(String),
       }),
       queryStringParameters: {
         testkey: "testvalue",
@@ -57,6 +57,72 @@ test("E2E Test", async () => {
     },
   };
 
-  // Validate the response against the expected structure
+  // Validate the response against the expected structure for GET
+  expect(responseData).toMatchObject(expected);
+});
+
+test("E2E Test for POST Request", async () => {
+  const response = await fetch(TEST_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ jsonkey: "jsonvalue" }),
+  });
+  const responseData = await response.json();
+
+  // Expected JSON for POST request
+  const expected = {
+    message: "Hello from Lambda!",
+    event: {
+      version: "2.0",
+      routeKey: "",
+      rawPath: "/foo/bar",
+      rawQueryString: "",
+      headers: expect.objectContaining({
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Content-Length": expect.any(String),
+        "User-Agent": expect.any(String),
+      }),
+      requestContext: {
+        routeKey: "",
+        accountId: "",
+        stage: "",
+        requestId: expect.any(String),
+        apiId: "",
+        domainName: "",
+        domainPrefix: "",
+        time: expect.any(String),
+        timeEpoch: expect.any(Number),
+        http: expect.objectContaining({
+          method: "POST",
+          path: "/foo/bar",
+          protocol: "HTTP/1.1",
+          sourceIp: expect.any(String),
+          userAgent: expect.any(String),
+        }),
+        authentication: expect.objectContaining({
+          clientCert: expect.any(Object),
+        }),
+      },
+      body: '{"jsonkey":"jsonvalue"}',
+      isBase64Encoded: false,
+    },
+    context: {
+      callbackWaitsForEmptyEventLoop: true,
+      functionVersion: "$LATEST",
+      functionName: "test_function",
+      memoryLimitInMB: "3008",
+      logGroupName: "/aws/lambda/Functions",
+      logStreamName: "$LATEST",
+      invokedFunctionArn:
+        "arn:aws:lambda:us-east-1:012345678912:function:test_function",
+      awsRequestId: expect.any(String),
+    },
+  };
+
+  // Validate the response against the expected structure for POST
   expect(responseData).toMatchObject(expected);
 });
